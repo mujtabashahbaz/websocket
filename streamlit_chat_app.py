12a26a214d0de8f83ba33ed02d6c1726ca91d7ff
Mujtaba Shahbaz <mujtaba@Mujtabas-MacBook-Pro.local>
2024-10-04 12:32:09 +0500

    third commit

--- streamlit_chat_app.py
+++ streamlit_chat_app.py
@@ -11,23 +11,26 @@ if 'user_id' not in st.session_state:
 
 st.title("Streamlit Chat App")
 
+# Function to add messages
+def add_message(sender, message):
+    st.session_state.messages.append((sender, message))
+
 # Display chat messages
 chat_container = st.container()
 
 # Input for new messages
-user_input = st.text_input("Type a message:")
+with st.form(key='message_form'):
+    user_input = st.text_input("Type a message:")
+    submit_button = st.form_submit_button(label='Send')
 
-if user_input:
+if submit_button and user_input:
     # Add user message to chat
-    st.session_state.messages.append(("You", user_input))
+    add_message("You", user_input)
     
     # Simulate receiving a message from another user
     other_user_id = f"User {random.randint(1000, 9999)}"
     response = f"This is a simulated response from {other_user_id}"
-    st.session_state.messages.append((other_user_id, response))
-    
-    # Clear the input box
-    st.experimental_rerun()
+    add_message(other_user_id, response)
 
 # Display all messages
 with chat_container:
